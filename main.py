c7233ad2ef5c333b953bf4f24d0a2bf8703fbd68
RT <sernazarovrustem@gmail.com>
2021-03-24 14:59:05 +0500

    first commit

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-#!/usr/bin/python3.6
 import telebot
 
-bot = telebot.TeleBot('1572674420:AAHz_p68py7VbSClOjK_RY0_m_P_bmWgsgc')
+bot = telebot.TeleBot("1572674420:AAHz_p68py7VbSClOjK_RY0_m_P_bmWgsgc")
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    send_mess = "<br>Hello {message.from_user.first_name}{message.from_user.last_name}</br>!"
-    bot.send_message(message.chat.id, send_mess, parse_mode = 'html')
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Howdy, how are you doing?")
 
-bot.polling(none_stop=True)
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+	bot.reply_to(message, message.text)
+
+bot.polling()
